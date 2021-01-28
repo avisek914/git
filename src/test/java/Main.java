253f82fe923cfed09504292282ff67706f554e37
avisek914 <avisek914@gmail.com>
2021-01-28 00:16:29 -0500

    Branch classes

---
+++ src/test/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        SolarSystem solarSystem=new SolarSystem();
+        solarSystem.sun=1;
+        solarSystem.moon=3;
+        solarSystem.planets=9;
+    }
+}
