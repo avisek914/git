f689216d5b838743c76d8b76706a07ba85df4a7e
Abhisek-Bhujel <74200704+Abhisek-Bhujel@users.noreply.github.com>
2021-01-29 01:02:23 -0500

    Update Main.java

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+         Abhisek Bhujel
+        
         SolarSystem solarSystem=new SolarSystem();
         solarSystem.sun=1;
         solarSystem.moon=3;
