46e6dec646e4fc448ea4c9d5374382e70af46ff8
avisek914 <avisek914@gmail.com>
2021-01-28 23:28:29 -0500

    Planet1 is ready to be merged

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -4,5 +4,11 @@ public class Main {
         solarSystem.sun=1;
         solarSystem.moon=3;
         solarSystem.planets=9;
+
+
+        Planet1 Fearo=new Planet1();
+        Fearo.color="Red";
+        Fearo.Radius="87889KM";
+        
     }
 }
